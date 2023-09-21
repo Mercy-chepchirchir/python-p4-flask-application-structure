cf39eda5def59a8cb9b581e706562ce3c028251e
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-09-22 00:16:21 +0300

    done

--- server/app.py
+++ server/app.py
@@ -8,16 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Welcome to my page!</h1>'
-# append to server/app.py
 
-@app.route('/<username>')
+
+@app.route('/<string:username>')
 def user(username):
     return f'<h1>Profile for {username}</h1>'
-# modify user() in server/app.py
 
-# @app.route('/<string:username>')
-#def user(username):
- #   return f'<h1>Profile for {username}</h1>'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
